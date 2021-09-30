845d260ddba5522cdd8f9ae4bdb86cd972d59e57
Colin Sidberry <colinsidberry@gmail.com>
2021-09-30 15:14:07 -0700

    implented code review suggestions and formatting. Started the invoices.js routes.

--- routes/companies.js
+++ routes/companies.js
@@ -11,7 +11,11 @@ const db = require("../db");
 /** GET / - returns array of all companies `{companies: [{code, name}, ...]}` */
 
 router.get("/", async function (req, res, next) {
-  const results = await db.query("SELECT code, name FROM companies");
+  const results = await db.query(
+    `SELECT code, name 
+        FROM companies
+        ORDER BY code`
+  );
   const companies = results.rows;
 
   return res.json({ companies });
@@ -23,7 +27,12 @@ router.get("/", async function (req, res, next) {
 router.get("/:code", async function (req, res, next) {
   const code = req.params.code;
   const results = await db.query(
-    "SELECT code, name, description FROM companies WHERE code = $1", [code]);
+    `SELECT code, name, description 
+        FROM companies 
+        WHERE code = $1
+        ORDER BY code`,
+    [code]
+  );
   const company = results.rows[0];
 
   if (!company) throw new NotFoundError(`No matching company: ${code}`);
@@ -35,11 +44,13 @@ router.get("/:code", async function (req, res, next) {
   *    {code, name, description} 
   * return `{company: {code, name, description}}` */
 router.post("/", async function (req, res, next) {
+  const { code, name, description } = req.body;
   const results = await db.query(
     `INSERT INTO companies (code, name, description)
          VALUES ($1, $2, $3)
          RETURNING code, name, description`,
-    [req.body.code, req.body.name, req.body.description]);
+    [code, name, description]
+  );
   const company = results.rows[0];
 
   return res.status(201).json({ company });
@@ -54,12 +65,14 @@ router.put("/:code", async function (req, res, next) {
   if ("code" in req.body) throw new BadRequestError("Not allowed");
 
   const code = req.params.code;
+  const { name, description } = req.body;
   const results = await db.query(
     `UPDATE companies
          SET name= $1, description = $2
          WHERE code = $3
-         RETURNING name, description`,
-    [req.body.name, req.body.description, code]);
+         RETURNING code, name, description`,
+    [name, description, code]
+  );
   const company = results.rows[0];
 
   if (!company) throw new NotFoundError(`No matching company: ${code}`);
@@ -72,7 +85,11 @@ router.put("/:code", async function (req, res, next) {
 router.delete("/:code", async function (req, res, next) {
   const code = req.params.code;
   const results = await db.query(
-    "DELETE FROM companies WHERE code = $1 RETURNING code", [code]);
+    `DELETE FROM companies 
+        WHERE code = $1 
+        RETURNING code`,
+    [code]
+  );
   const company = results.rows[0];
 
   if (!company) throw new NotFoundError(`No matching company: ${code}`);
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,7 +1,6 @@
 "use strict";
 /** Routes about invoices. */
 
-
 const express = require("express");
 const { NotFoundError, BadRequestError } = require("../expressError");
 
@@ -9,6 +8,88 @@ const router = new express.Router();
 const db = require("../db");
 
 
+/** GET / - returns array of all invoices `{invoices: [{id, comp_code}, ...]}` */
+
+router.get("/", async function (req, res, next) {
+    const results = await db.query("SELECT id, comp_code FROM invoices");
+    const invoices = results.rows;
+
+    return res.json({ invoices });
+});
+
+
+/** GET /[id] - return data about one invoice: 
+ * {
+ *      invoice: {id, amt, paid, add_date, paid_date, 
+ *          company: {code, name, description}
+ *      }
+ * }
+ */
+
+router.get("/:id", async function (req, res, next) {
+    const id = req.params.id;
+    const invoiceResults = await db.query(
+        "SELECT id, code, amt, paid, add_date, paid_date FROM invoices WHERE id = $1", [id]);
+    const code = results.row[0].code;
+    const invoice = results.rows[0];
+    delete invoice.code;
+
+    const companyResults = await db.query(
+        "SELECT code, name, description FROM company WHERE id = $1", [id]);
+    const invoice = results.rows[0];
+
+    if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
+    return res.json({ invoice });
+});
+
+
+/** POST / - create invoice from data formatted like below:
+  *    {id, name, description} 
+  * return `{invoice: {id, name, description}}` */
+router.post("/", async function (req, res, next) {
+    const results = await db.query(
+        `INSERT INTO invoices (id, name, description)
+         VALUES ($1, $2, $3)
+         RETURNING id, name, description`,
+        [req.body.id, req.body.name, req.body.description]);
+    const invoice = results.rows[0];
+
+    return res.status(201).json({ invoice });
+});
+
+
+/** PUT /[id] - update name and description in a invoice; 
+ * return `{invoice: {id, name, description}}` 
+ */
+
+router.put("/:id", async function (req, res, next) {
+    if ("id" in req.body) throw new BadRequestError("Not allowed");
+
+    const id = req.params.id;
+    const results = await db.query(
+        `UPDATE invoices
+         SET name= $1, description = $2
+         WHERE id = $3
+         RETURNING name, description`,
+        [req.body.name, req.body.description, id]);
+    const invoice = results.rows[0];
+
+    if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
+    return res.json({ invoice });
+});
+
+
+/** DELETE /[id] - delete a invoice, return `{status: "deleted"}` */
+
+router.delete("/:id", async function (req, res, next) {
+    const id = req.params.id;
+    const results = await db.query(
+        "DELETE FROM invoices WHERE id = $1 RETURNING id", [id]);
+    const invoice = results.rows[0];
+
+    if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
+    return res.json({ status: "deleted" });
+});
 
 
 module.exports = router;
